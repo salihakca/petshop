3e6f3895534ebded3bfbb3484c790d9c2ecf4a6a
salihakca <101511605+salihakca@users.noreply.github.com>
2024-06-04 21:15:41 +0300

    search screen list fixed

--- lib/searchscreen.dart
+++ lib/searchscreen.dart
@@ -27,7 +27,246 @@ class _SearchScreenState extends State<SearchScreen> {
       'image': 'assets/photo/cat.jpeg',
       'price': '799 TL',
     },
-    // Diğer öğeler burada devam ediyor...
+     {
+      'name': 'Kurutulmuş Kedi Maması',
+      'image': 'assets/photo/cat.jpeg',
+      'price': '799 TL',
+    },
+    {
+      'name': 'Yetişkin Kedi Maması',
+      'image': 'assets/photo/cat_mama1.png',
+      'price': '999 TL',
+    },
+    {
+      'name': 'Yavru Kedi Maması',
+      'image': 'assets/photo/cat_mama2.png',
+      'price': '899 TL',
+    },
+    {
+      'name': 'Yetişkin Kedi Maması',
+      'image': 'assets/photo/cat_mama3.png',
+      'price': '300 TL',
+    },
+    {
+      'name': 'Kuzu Etli Maması',
+      'image': 'assets/photo/cat_mama4.png',
+      'price': '100 TL',
+    },
+    {
+      'name': 'Ringa Balıklı Mama',
+      'image': 'assets/photo/cat_mama5.png',
+      'price': '200 TL',
+    },
+    {
+      'name': 'Kısır Kedi Maması',
+      'image': 'assets/photo/cat_mama6.png',
+      'price': '155 TL',
+    },
+    {
+      'name': 'Kuzu Etli Mama',
+      'image': 'assets/photo/cat_mama7.png',
+      'price': '15 TL',
+    },
+    {
+      'name': 'Yaş Kedi Maması',
+      'image': 'assets/photo/cat_mama8.png',
+      'price': '25 TL',
+    },
+    {
+      'name': 'Yaşlı Kedi Maması',
+      'image': 'assets/photo/cat_mama9.png',
+      'price': '35 TL',
+    },
+    {
+      'name': 'Ergen Yaş Maması',
+      'image': 'assets/photo/cat_mama10.png',
+      'price': '45 TL',
+    },
+    {
+      'name': 'Karışık Papağan Yemi',
+      'image': 'assets/photo/papagan1.png',
+      'price': '22 TL',
+    },
+    {
+      'name': 'Karışık Papağan Yemi',
+      'image': 'assets/photo/papagan2.png',
+      'price': '33 TL',
+    },
+    {
+      'name': 'Cennet Papağanı Yemi',
+      'image': 'assets/photo/papagan3.png',
+      'price': '44 TL',
+    },
+    {
+      'name': 'Karışık Mama',
+      'image': 'assets/photo/papagan4.png',
+      'price': '55 TL',
+    },
+    {
+      'name': 'Meyveli Yem',
+      'image': 'assets/photo/papagan5.png',
+      'price': '66 TL',
+    },
+    {
+      'name': 'Papağan Çekirdeği',
+      'image': 'assets/photo/papagan6.png',
+      'price': '77 TL',
+    },
+
+    {
+      'name': 'Balık Yemi',
+      'image': 'assets/photo/balık1.png',
+      'price': '88 TL',
+    },
+    {
+      'name': 'Narlı Yem',
+      'image': 'assets/photo/balık2.png',
+      'price': '13 TL',
+    },
+    {
+      'name': 'Küçük Balık Yemi',
+      'image': 'assets/photo/balık3.png',
+      'price': '15 TL',
+    },
+    {
+      'name': 'Büyük Balık Yemi',
+      'image': 'assets/photo/balık4.png',
+      'price': '16 TL',
+    },
+    {
+      'name': 'Atlı Balık Yemi',
+      'image': 'assets/photo/balık5.png',
+      'price': '24 TL',
+    },
+    {
+      'name': 'Balık Yemi',
+      'image': 'assets/photo/balık6.png',
+      'price': '27 TL',
+    },
+
+     {
+      'name': 'Kuru Lonca',
+      'image': 'assets/photo/tavsan1.png',
+      'price': '28 TL',
+    },
+    {
+      'name': 'Kemirgen Yemi',
+      'image': 'assets/photo/tavsan2.png',
+      'price': '31 TL',
+    },
+    {
+      'name': 'Kemirgen Yemi',
+      'image': 'assets/photo/tavsan3.png',
+      'price': '32 TL',
+    },
+    {
+      'name': 'Kemirgen Tahışı',
+      'image': 'assets/photo/tavsan4.png',
+      'price': '33 TL',
+    },
+    {
+      'name': 'Tavşan Yemi',
+      'image': 'assets/photo/tavsan5.png',
+      'price': '43 TL',
+    },
+    {
+      'name': 'Tavşan Yemi',
+      'image': 'assets/photo/tavsan6.png',
+      'price': '45 TL',
+    },
+     {
+      'name': 'Karışık Papağan Yemi',
+      'image': 'assets/photo/papagan1.png',
+      'price': '22 TL',
+    },
+    {
+      'name': 'Karışık Papağan Yemi',
+      'image': 'assets/photo/papagan2.png',
+      'price': '33 TL',
+    },
+    {
+      'name': 'Cennet Papağanı Yemi',
+      'image': 'assets/photo/papagan3.png',
+      'price': '44 TL',
+    },
+    {
+      'name': 'Karışık Mama',
+      'image': 'assets/photo/papagan4.png',
+      'price': '55 TL',
+    },
+    {
+      'name': 'Meyveli Yem',
+      'image': 'assets/photo/papagan5.png',
+      'price': '66 TL',
+    },
+    {
+      'name': 'Papağan Çekirdeği',
+      'image': 'assets/photo/papagan6.png',
+      'price': '77 TL',
+    },
+
+    {
+      'name': 'Köpek Tasması',
+      'image': 'assets/photo/urun1.png',
+      'price': '151 TL',
+    },
+    {
+      'name': 'Kedi Tuvaleti',
+      'image': 'assets/photo/urun2.png',
+      'price': '141 TL',
+    },
+    {
+      'name': 'Göğüs Tasması',
+      'image': 'assets/photo/urun3.png',
+      'price': '131 TL',
+    },
+    {
+      'name': 'KK Çantası',
+      'image': 'assets/photo/urun4.png',
+      'price': '141 TL',
+    },
+    {
+      'name': 'Aparat + Tüy Toplayıcı',
+      'image': 'assets/photo/urun5.png',
+      'price': '161 TL',
+    },
+    {
+      'name': 'Papağan Kafesi',
+      'image': 'assets/photo/urun6.png',
+      'price': '121 TL',
+    },
+
+     {
+      'name': 'Kuru Lonca',
+      'image': 'assets/photo/tavsan1.png',
+      'price': '28 TL',
+    },
+    {
+      'name': 'Kemirgen Yemi',
+      'image': 'assets/photo/tavsan2.png',
+      'price': '31 TL',
+    },
+    {
+      'name': 'Kemirgen Yemi',
+      'image': 'assets/photo/tavsan3.png',
+      'price': '32 TL',
+    },
+    {
+      'name': 'Kemirgen Tahışı',
+      'image': 'assets/photo/tavsan4.png',
+      'price': '33 TL',
+    },
+    {
+      'name': 'Tavşan Yemi',
+      'image': 'assets/photo/tavsan5.png',
+      'price': '43 TL',
+    },
+    {
+      'name': 'Tavşan Yemi',
+      'image': 'assets/photo/tavsan6.png',
+      'price': '45 TL',
+    },
+
   ];
 
   List<Map<String, dynamic>> _filteredResults = [];
